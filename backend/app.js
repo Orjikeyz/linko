74cdffd1577fc33b6060bf8b20fe902492012dab
Xcode <Orjikeyz7@gmail.com>
2026-03-06 13:33:57 +0100

    updated

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const cookieParser = require("cookie-parser");
 
 
 app.use(cors({
-    origin: ["http://localhost", "http://127.0.0.1:5500"],
+    origin: ["http://localhost", "http://127.0.0.1:5500", "https://linko-ng.vercel.app/"],
     credentials: true
 }));
 

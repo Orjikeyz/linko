84b43917e2055059bcdf1b5c91f0918c689a24b1
Xcode <Orjikeyz7@gmail.com>
2026-06-22 11:56:07 +0100

    updated

--- frontend/assets/url.js
+++ frontend/assets/url.js
@@ -1,3 +1,5 @@
-let productUrl = "linko-ng.vercel.app/" //producturl same as frontend url
-let backendUrl = "https://linko-mosc.onrender.com"
-// let backendUrl = "http://localhost:3000" 
+// let productUrl = "https://linko-ng.vercel.app/" //producturl same as frontend url
+// let backendUrl = "https://linko-mosc.onrender.com"
+
+let productUrl = "http://localhost/linko" //producturl same as frontend url
+let backendUrl = "http://localhost:3000" 
--- frontend/assets/urlParams.js
+++ frontend/assets/urlParams.js
@@ -1,6 +1,6 @@
 let params = new URLSearchParams(window.location.search)
 const paramsValue = params.get('id') || ""
-// const productId = params.get('pid') || ""
+const pid = params.get('pid') || "" // product Id
 
 if (paramsValue === '' || !paramsValue) {
     document.getElementsByTagName("body")[0].style.display = "none"

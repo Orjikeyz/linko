6d2198277046cdace34cc948678822e7fab51c72
Xcode <Orjikeyz7@gmail.com>
2026-06-22 12:07:39 +0100

    update

--- frontend/assets/url.js
+++ frontend/assets/url.js
@@ -1,5 +1,5 @@
-let productUrl = "https://linko-ng.vercel.app/" //producturl same as frontend url
-let backendUrl = "https://linko-mosc.onrender.com"
+// let productUrl = "https://linko-ng.vercel.app/" //producturl same as frontend url
+// let backendUrl = "https://linko-mosc.onrender.com"
 
-// let productUrl = "http://localhost/linko" //producturl same as frontend url
-// let backendUrl = "http://localhost:3000" 
+let productUrl = "http://localhost/linko" //producturl same as frontend url
+let backendUrl = "http://localhost:3000" 
